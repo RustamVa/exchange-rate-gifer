58bcf1a85b1adbd00aced84be196c71c0ffce69a
RustamVa <loveiswhat2208@gmail.com>
2022-06-16 23:24:09 +0500

    init

---
+++ src/test/java/ru/alfabank/exrg/ExrgApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alfabank.exrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
